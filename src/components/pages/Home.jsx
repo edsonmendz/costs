3ab38d24020b26a21b060573705c2977a5c77233
edsonmendz <japaact@gmail.com>
2024-02-28 20:40:23 -0300

    Home Finalizado

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -6,7 +6,7 @@ import './home.css'
 
 function Home() {
     return (
-        <section>
+        <section className='homeContainer'>
             <h1>Bem-vindo ao <span>Costs</span></h1>
             <p>Comece a gerenciar os seus projetos agora mesmo!</p>
             <LinkButton to="/newproject" text="Criar projeto" />
