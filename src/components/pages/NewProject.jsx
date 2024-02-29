c5c548fab8dac5573e66904fbe4087e282015c56
edsonmendz <japaact@gmail.com>
2024-02-29 06:24:17 -0300

    na metade

--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,6 +1,12 @@
+import './NewProject.css'
+
 function NewProject() {
     return (
-        <h1>NewProject</h1>
+        <div className='newProject_container'>
+            <h1>Criar Projeto</h1>
+            <p>Crie seus projetos para depois adicionar os serviços</p>
+            <p>formulario</p>
+        </div>
     )
 }
 
---
+++ src/components/project/ProjectForm.jsx
@@ -0,0 +1,9 @@
+function ProjectForm() {
+    return (
+        <div className=''>
+            
+        </div>
+    )
+}
+
+export default ProjectForm;
