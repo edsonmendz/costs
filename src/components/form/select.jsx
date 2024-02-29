9441b98ac5fe4649da11e69fbbf8979515492f09
edsonmendz <japaact@gmail.com>
2024-02-29 12:48:38 -0300

    metade

--- src/components/form/select.jsx
+++ src/components/form/select.jsx
@@ -1,11 +1,14 @@
 import './select.css'
 
-function Select({ type, text, name, placeholder, handleOnchange, value}) {
+function Select({ text, name, options, handleOnchange, value}) {
     return (
         <div className='form_control'>
             <label htmlFor={name}>{text}</label>
             <select name={name} id={name}>
                 <option>Selecione uma opção</option>
+                {options.map((option) => (
+                    <option value={option.id} key={option.id}>{option.name}</option>
+                ))}
             </select>
         </div>
     )
--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -2,8 +2,28 @@ import './ProjectForm.css'
 import Input from '../form/input';
 import Select from '../form/select';
 import SubmitButton from '../form/SubmitButton';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function ProjectForm({btnText}) {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then((resposta) => resposta.json())
+    .then((data) => {
+        setCategories(data)
+    })
+    .catch((erro) => console.log(erro))
+    }, [])
+
+
     return (
         <form className='form'>
             <div>
@@ -13,7 +33,10 @@ function ProjectForm({btnText}) {
                 <Input type={'number'} text={'Orçamento do Projeto'} name='budget' placeholder={'Orçamento total'}></Input>
             </div>
             <div>
-                <Select name='category_id' text='Selecione a categoria' />
+                <Select 
+                name='category_id' 
+                text='Selecione a categoria' 
+                options={categories} />
             </div>
             <div>
                 <SubmitButton text={btnText} />
